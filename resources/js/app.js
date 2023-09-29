8dafd81a1cd9154923a6a1372cc877d22056c305
ZaynOssama <oussamazazou@iesmontsia.org>
2023-09-29 16:18:24 +0200

    React Working with Breeze

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,5 @@
 import './bootstrap';
+import './components/App';
 
 import Alpine from 'alpinejs';
 
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
+Route::get('/', function () {
+    return view('auth.login');
+});
+
+// Route::get('/test', function () {
 //     return view('welcome');
 // });
 
