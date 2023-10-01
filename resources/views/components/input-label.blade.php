44920f93b892bed1891868ab0af2dfd9f07ce261
ZaynOssama <oussamazazou@iesmontsia.org>
2023-10-01 19:30:54 +0200

    The end. (for today)

--- resources/views/components/input-label.blade.php
+++ resources/views/components/input-label.blade.php
@@ -1,5 +1,5 @@
 @props(['value'])
 
-<label {{ $attributes->merge(['class' => 'block font-medium text-sm text black-700  text black-300']) }}>
+<label {{ $attributes->merge(['class' => 'block font-medium text-sm text black-700 text black-300']) }}>
     {{ $value ?? $slot }}
 </label>
