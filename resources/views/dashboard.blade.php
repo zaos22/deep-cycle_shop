20eddbb812d18b08d43bf3271853156d232821fd
ZaynOssama <oussamazazou@iesmontsia.org>
2023-10-01 19:04:55 +0200

    Changed title

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,9 +1,12 @@
 <x-app-layout>
-    <div class="text-center max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-        <h1 class="font-semibold text-4xl text black-800  text black-200 leading-tight">
-            {{ __('Menu') }}
-        </h1>
-    </div>
+    <x-slot name="header">
+        <div class="text-center">
+            <h1 class="font-semibold text-xl text black-800  text black-200 leading-tight">
+                {{ __('Menu') }}
+            </h1>
+        </div>
+    </x-slot>
+
     <div class="py-7">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="colorbg1 overflow-hidden shadow-sm sm:rounded-lg">
