4972516354c2b919d4862ac97a26db39cee992a5
ZaynOssama <oussamazazou@iesmontsia.org>
2023-10-27 18:07:30 +0200

    Pdf Bill Done!!

--- resources/js/components/Bills/Bills.jsx
+++ resources/js/components/Bills/Bills.jsx
@@ -1,37 +1,105 @@
-import react from "react";
+import React, { useEffect, useState } from "react";
 import Button from 'react-bootstrap/Button';
-import Swal from 'sweetalert2'
 import axios from "axios";
 import Bill_Lines from './Bill_Lines';
 import { createRoot } from "react-dom/client";
-import { useEffect, useState } from "react";
-
+import jsPDF from "jspdf";
+import 'jspdf-autotable';
 
 export default function Bills() {
-    const [bills, setBills] = useState([])
+    const [bills, setBills] = useState([]);
     const [search, setSearch] = useState("");
 
     useEffect(() => {
-        getBills()
-    }, [])
+        getBills();
+    }, []);
 
     const getBills = async (searchTerm) => {
         try {
-            // Si searchTerm es null, obtiene todos los proveedores
             const response = await axios.get(`/bills-data?search=${searchTerm || ''}`);
             setBills(response.data);
         } catch (error) {
-            console.error('Error al obtener proveedores:', error);
+            console.error('Error al obtener facturas:', error);
+        }
+    };
+
+    const getBillLines = async (billId) => {
+        try {
+            const response = await axios.get(`/bill_lines/${billId}`);
+            return response.data;
+        } catch (error) {
+            console.error('Error al obtener líneas de factura:', error);
+            return [];
         }
     };
 
     const handleSearchChange = (event) => {
         const searchTerm = event.target.value;
         setSearch(searchTerm);
-
-        // Llama a la función para obtener proveedores
-        // Si el campo de búsqueda está vacío, no se especifica un término de búsqueda
         getBills(searchTerm === '' ? null : searchTerm);
+    };
+
+    const generatePDF = async (bill) => {
+        const doc = new jsPDF();
+
+        doc.setFontSize(12);
+
+        // Título "Bill"
+        doc.setFont('helvetica', 'bold');
+        doc.setFontSize(18);
+        doc.text('Bill', 105, 15);
+        doc.text(' ', 105, 15);
+
+        // Información de la factura
+        doc.setFontSize(10);
+        doc.setFont('helvetica', 'normal');
+        // Linia con nombre de factura y fecha
+        doc.text(`Bill Name: ${bill.name}`, 10, 30);
+        doc.text(`Date: ${bill.date == null ? 'Pending' : bill.date.substring(0, 10)}`, 160, 30);
+        // Linia con cliente y proveedor
+        doc.text(`Client: ${bill.client}`, 10, 50);
+        doc.text(`Supplier: ${bill.supplier}`, 160, 50);
+
+        // Obtener líneas de factura
+        const billLines = await getBillLines(bill.id);
+
+        const columns = ["Unity", "Description", "Price"];
+        const rows = [];
+
+        billLines.forEach((line) => {
+            rows.push([
+                line.unity,
+                `${line.product_name == null ? line.material_name : line.product_name}`,
+                line.unity !== 1 ? bill.total / 5 : bill.total,
+            ]);
+        });
+
+        doc.autoTable({
+            head: [columns],
+            body: rows,
+            startY: 70,
+            theme: "striped",
+            styles: { halign: "center" },
+        });
+
+        // Calcular y agregar totales
+        const subtotal = bill.total;
+        const tax = 0.25 * subtotal; // 25% de impuestos
+        const grandTotal = subtotal + tax;
+
+        // Totales
+        doc.setFontSize(14);
+        doc.text(`Subtotal: €${subtotal.toFixed(2)}`, 10, doc.autoTable.previous.finalY + 20);
+        doc.text(`Tax (25%): €${tax.toFixed(2)}`, 10, doc.autoTable.previous.finalY + 30);
+        doc.setFont('helvetica', 'bold');
+        doc.text(`Grand Total: €${grandTotal.toFixed(2)}`, 10, doc.autoTable.previous.finalY + 40);
+
+        // Genera una URL de datos del PDF
+        const pdfDataUri = doc.output('datauristring');
+
+        // Abre una nueva pestaña con la URL de datos del PDF
+        const newTab = window.open();
+        newTab.document.write('<embed width="100%" height="100%" src="' + pdfDataUri + '" type="application/pdf">');
     };
 
 
@@ -52,24 +120,13 @@ export default function Bills() {
             <table className="table-auto table-hover w-full shadow-lg rounded-lg">
                 <thead>
                     <tr className="colorbg shadow-lg rounded-lg text-black">
-                    <th className="px-6 py-3 uppercase">
-                            Bill Name
-                        </th>
-                        <th className="px-6 py-3 uppercase">
-                            Client
-                        </th>
-                        <th className="px-6 py-3 uppercase">
-                            Supplier
-                        </th>
-                        <th className="px-6 py-3 uppercase">
-                            Date
-                        </th>
-                        <th className="px-6 py-3 uppercase">
-                            Bill Lines
-                        </th>
-                        <th className="px-6 py-3 uppercase">
-                            Total
-                        </th>
+                        <th className="px-6 py-3 uppercase">Bill Name</th>
+                        <th className="px-6 py-3 uppercase">Client</th>
+                        <th className="px-6 py-3 uppercase">Supplier</th>
+                        <th className="px-6 py-3 uppercase">Date</th>
+                        <th className="px-6 py-3 uppercase">Bill Lines</th>
+                        <th className="px-6 py-3 uppercase">Total</th>
+                        <th className="px-6 py-3 uppercase">Bill</th>
                     </tr>
                 </thead>
                 <tbody className="custom-tbody text-center">
@@ -81,6 +138,9 @@ export default function Bills() {
                             <td>{bill.date == null ? 'Pending' : bill.date.substring(0, 10)}</td>
                             <td><Bill_Lines billID={bill.id}></Bill_Lines></td>
                             <td>{bill.total}</td>
+                            <td>
+                                <Button variant="primary" onClick={() => generatePDF(bill)}><i className="fa-solid fa-download"></i></Button>
+                            </td>
                         </tr>
                     ))}
                 </tbody>
