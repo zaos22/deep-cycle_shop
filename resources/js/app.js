76a3868c2a02515537ecb210da0794d7c5772adb
ZaynOssama <oussamazazou@iesmontsia.org>
2023-12-18 19:23:30 +0100

    Deleted component from app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,7 +5,6 @@ import './components/Suppliers/Suppliers';
 import './components/Products/Products';
 import './components/Bills/Bills';
 import './components/Shop/Client';
-import './components/Shop/Check';
 
 import Alpine from 'alpinejs';
 import 'bootstrap/dist/css/bootstrap.min.css';
